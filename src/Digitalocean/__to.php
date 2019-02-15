f0952575ef2f4ec9b1a8b03bf118cd14c9d97532
xltxlm <xltxlm@qq.com>
2019-02-15 13:02:34 +0800

    添加digitalocean接口

--- src/Digitalocean/__to.php
+++ src/Digitalocean/__to.php
@@ -10,9 +10,14 @@ Trait __to
 {
     use __to\__to_implements;
 
+    /**
+     * @param string $remote_path ;
+     * @return $this
+     */
     public function setremote_path(string $remote_path = "")
     {
-        $this->setremote_path(ltrim($remote_path, '/'));
+        $this->remote_path = (ltrim($remote_path, '/'));
+        return $this;
     }
 
 
